dfda1c1165c5eb5651a79b482fa27b2e5dc0fcc0
edbalogh <ed.balogh@gmail.com>
2021-09-11 13:15:30 -0400

    test

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const prompt = require('prompt-sync');
+
 // run `node index.js` in the terminal
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+// console.log(`Hello Node.js v${process.versions.node}!`);
+const name = prompt('What is your name?');
+console.log(`hello ${name}!`);
